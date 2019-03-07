3ee7775789e9eba0b2ae88f1edd7167815103314
dafne <dafne4696@gmail.com>
2019-03-07 14:48:53 +0200

    minor

--- Scaffold_Code/block.py
+++ Scaffold_Code/block.py
@@ -1,17 +1,30 @@
 import blockchain
 
 class Block:
-	def __init__(self, previousHash, lista):
+	def __init__(self, previousHash, currentbl):
 		##set
 
 		self._previousHash = previousHash
 		self._timestamp =
-		self.listOfTransactions = lista
-		self.nonce = 
+		self.listOfTransactions = []
+		self.blocknumber=currentbl.getblocknum()+1
+		self.nonce = ""
+
+	def getblocknum(self):
+		return blocknumber
+
+		
+	def add_nonce(self,n):
+		self.nonce = n
 
 	def myHash:
 		#calculate self.hash
+	
+	def to_dict(self):
+        return OrderedDict({'transactions': self.listOfTransactions,
+                            'previousHash': self._previousHash,
+                            'nonce': self.nonce})
 
 
-	def add_transaction(self , transaction):
+	#def add_transaction(self , transaction):
 		#add a transaction to the block
---
+++ Scaffold_Code/chain.py
@@ -0,0 +1,13 @@
+
+from Scaffold_Code import block.py , wallet.py , config.py 
+
+
+
+
+
+
+
+def validate_chain(self):
+
+def add_block_to_mychain(block):
+		self.chain
