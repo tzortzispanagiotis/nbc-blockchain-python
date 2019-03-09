7437d6aadad2b5a68f07afe3c9d1cd158cb67e70
dafne <dafne4696@gmail.com>
2019-03-09 12:29:53 +0200

    transaction.py code

--- Scaffold_Code/transaction.py
+++ Scaffold_Code/transaction.py
@@ -1,32 +1,40 @@
 from collections import OrderedDict
 
 import binascii
-
+import hashlib
+import json
 import Crypto
 import Crypto.Random
 from Crypto.Hash import SHA
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
-
 import requests
 from flask import Flask, jsonify, request, render_template
 
 class TransactionInput:
 #input points to output
-    def __init__(self, transaction, output_idx):
-        previousOutputId = transaction.
-
-class TransactionOutput:
-    def __init__(self, transaction, amount):
-        self.recipient = transaction.receiver_address
-        self.amount = amount
-        self.id = #το hash
-        self.transactionId = transaction.id
+    def __init__(self, transactionoutput):
+        self.parentOutputId = transactionoutput.transactionId
 
     def to_dict(self):
         d = {
-            'sender_address': self.sender_address
-            'recipient_address': self.recipient,
+            'parentTransaction':self.parentOutputId 
+        }
+        return d 
+
+class TransactionOutput:
+    def __init__(self, receiver_address, amount):
+        self.recipient =receiver_address
+        self.amount = amount
+        self.transactionId = None
+
+    def fill_id(self , tid):
+        self.transactionId=tid
+
+    def to_dict(self):
+        d = {
+            #'sender_address': self.sender_address
+            'recipient': self.recipient,
             'amount': self.amount
 
         }
@@ -35,39 +43,54 @@ class TransactionOutput:
 
 class Transaction:
 
-    def __init__(self, wallet, recipient_address, value):
+    def __init__(self, wallet, recipient_address, value,UTXOS ,):
 
         #set
 
-
-        self.sender_address = wallet.address #To public key του wallet από το οποίο προέρχονται τα χρήματα
+        self.wallet=wallet
+        self.sender_address = wallet.getAddress() #To public key του wallet από το οποίο προέρχονται τα χρήματα
         self.receiver_address = recipient_address #To public key του wallet στο οποίο θα καταλήξουν τα χρήματα
         self.amount = value #το ποσό που θα μεταφερθεί
-        self.transaction_id =  #το hash του transaction
-        self.transaction_inputs: #λίστα από Transaction Input
-        self.transaction_outputs: #λίστα από Transaction Output
-        self.signature
+        self.transaction_inputs= UTXOS
+        self.transaction_outputs=self.createOutputs()#λίστα από Transaction Output
+        self.signature =wallet.sign_transaction(self.to_dict1(False))
+        self.transaction_id =self.hash_transaction() #το hash του transaction
 
 
-    
+
+    def createOutputs(self):
+        out=[]
+        #output gia ton reciever
+        output1=TransactionOutput(self.receiver_address , self.amount)
+        #afta pou perisepsan se afton pou esteile
+        output2=TransactionOutput(self.sender_address , self.wallet.get_balance())
+        out.append(output1)
+        out.append(output2)
+        return out
+
+    def getid(self):
+        return self.transaction_id 
+
 
 
     def to_dict1(self,include_signature=True):
         d = {
-            "inputs": list(map(TransactionInput.to_dict, self.inputs)),
-            "outputs": list(map(TransactionOutput.to_dict, self.outputs)),
-        
+            "inputs": list(map(TransactionInput.to_dict, self.transaction_inputs)),
+            "outputs": list(map(TransactionOutput.to_dict, self.transaction_outputs)),
         }
+        #to message pou upografw kai meta elegxw an antistoixei stin upografi apoteleitai mono apo ta 
+        #inputs kai ta outputs
         if include_signature:
             d["signature"] = self.signature
         return d
         
+    def hash_transaction(self):
+         return  hashlib.sha256(json.dumps(self.to_dict1(True)))
 
-    def sign_transaction(self):
-        """
-        Sign transaction with private key
-        """
-       private_key = RSA.importKey(binascii.unhexlify(self.sender_private_key))
-       signer = PKCS1_v1_5.new(private_key)
-       h = SHA.new(str(self.to_dict()).encode('utf8'))
-       return binascii.hexlify(signer.sign(h)).decode('ascii')
+
+    def add_id_to_output(self):
+        for out in self.transaction_outputs:
+            out.fill_id(self.transaction_id)
+
+    #mallon na paei sto walltet
+   
