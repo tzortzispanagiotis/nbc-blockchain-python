e2c92943391d83c72550ba328d70e3d167e1b83e
dafne <dafne4696@gmail.com>
2019-03-12 13:35:55 +0200

    changes at node and block

--- Scaffold_Code/block.py
+++ Scaffold_Code/block.py
@@ -1,14 +1,17 @@
 import blockchain
-
+import hashlib
+import json
+import datetime
 class Block:
 	def __init__(self, previousHash, currentbl):
 		##set
 
 		self._previousHash = previousHash
-		self._timestamp =
+		self._timestamp =datetime.datetime.now()
 		self.listOfTransactions = []
 		self.blocknumber=currentbl.getblocknum()+1
 		self.nonce = ""
+		self.currenthash = None
 
 	def getblocknum(self):
 		return blocknumber
@@ -17,13 +20,22 @@ class Block:
 	def add_nonce(self,n):
 		self.nonce = n
 
-	def myHash:
+	def myHash(self):
+         h=hashlib.sha256(json.dumps(self.to_dict()))
+		 self.currenthash = h
+		 return h 
+
 		#calculate self.hash
-	
+	def getHash(self):
+		return self.currenthash
 	def to_dict(self):
-        return OrderedDict({'transactions': self.listOfTransactions,
-                            'previousHash': self._previousHash,
-                            'nonce': self.nonce})
+        d = {
+			'transactions': self.listOfTransactions,
+			'previousHash': self._previousHash,
+			'nonce': self.nonce , 
+			'number':self.blocknumber
+		}					
+	    return d
 
 
 	#def add_transaction(self , transaction):
--- Scaffold_Code/node.py
+++ Scaffold_Code/node.py
@@ -8,7 +8,8 @@ class Node: #creation of bootstap node
 		self.chain = []
 		#self.current_id_count
 		self.wallet = create_wallet()
-		self.transaction_pool
+		self.transaction_pool = []
+		#utxo==transaction_output
 		self.UTXO = []
 		#slef.ring[]   #here we store information for every node, as its id, its address (ip:port) its public key and its balance 
 
@@ -27,7 +28,7 @@ class Node: #creation of bootstap node
 	def register_node_to_ring():
 		#add this node to the ring, only the bootstrap node can add a node to the ring after checking his wallet and ip:port address
 		#bottstrap node informs all other nodes and gives the request node an id and 100 NBCs
-        return True 
+        #return True 
 
 	def create_transaction(sender, receiver,amount ,  signature , wallet):
 		traninput = []
@@ -35,8 +36,10 @@ class Node: #creation of bootstap node
 		for i in UTXO:
 			if (i.recepient==sender):
 				traninput.append(i)
+				UTXO.remove(i)
         new_transaction = Transaction(wallet , receiver , amount , traninput)
-		new_transaction.add_id_to_output()	    
+		new_transaction.add_id_to_output()	
+		return new_transaction    
 		#remember to broadcast it
 
 
@@ -45,6 +48,7 @@ class Node: #creation of bootstap node
 
 	def verify_signature(transaction):
 		tr = transaction.to_dict1(False)
+		#analoga se ti morfi tha to pairnw , ligika tha einai idi se json
 		pubkey= RSA.importKey(binascii.unhexlify(tr)
 	    verifier = PKCS1_v1_5.new(pubkey)
 		h = SHA.new(message.encode('utf8'))
@@ -53,21 +57,24 @@ class Node: #creation of bootstap node
 
 
 	def validate_transaction(transaction):
-		#when receiving a transaction, first validate with the signature
-		#second, check if UTXOs are enough to make the transaction
-		#if validation is OK, add transaction to current block
+		#sos ti object einai to transaction tha einai logika se morfi dict?
 		if (verify_signature(transaction)):
-
-			if not isinstance(transaction, GenesisTransaction): {
-
-			# Verify input transactions
-			for tx in transaction.inputs:
-				if not validate_transaction(tx.transaction): 
-					logging.error("Invalid parent transaction")
-					return False
-			}
-			return True
-		return False
+			traninput=[]
+			sum1=0
+			for i in UTXO:
+				if (i.recepient==transaction.sender_address):
+					traninput.append(i)
+					sum1=sum1+i.amount
+			if (sum1>=transaction.amount)
+			#eparki xrimata gia tin metafora
+				for t in traninput:
+					UTXO.remove(t)
+				#now create transaction outputs and add them at the utxo list
+				out1=TransactionOutput(transaction.receiver_address , transaction.amount)
+				out2=TransactionOutput(transaction.sender_address ,sum1-amount)
+				UTXO.append(out1)
+				UTXO.append(out2)
+		
 
 	def add_transaction_to_block(current_block , transaction , previousHash): 
 		#if
@@ -76,6 +83,7 @@ class Node: #creation of bootstap node
 		{
 			if (len(current_block) == max_transactions):
 				new_block = Block(previousHash , current_block)
+				new_block.myHash()
 				mine_block(new_block)
 			
 		    else:
@@ -112,6 +120,13 @@ class Node: #creation of bootstap node
 
 
 	#concencus functions
+    def validate_block(self ,block):
+		#check proof of work 
+		#check previous hash
+		my_last_block=self.chain[-1]
+		 if (block['previous_hash'] != my_last_block.gethash()):
+			 resolve_conflicts(self)
+		 else return True
 
 	def valid_chain(self, chain):
 		#check for the longer chain accroose all nodes
